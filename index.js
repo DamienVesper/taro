d8e428a8db99c7015fc132da7bd24ea167fb0e96
Damien Vesper <ldamienvesper@gmail.com>
2021-02-26 17:18:39 -0600

    ethically handle errors

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+require(`dotenv`).config();
+const config = require(`./config/config.js`);
+
+// Log errors.
+const log = require(`./src/server/utils/log.js`);
+process.on(`uncaughtException`, e => {
+    log(`red`, e.stack);
+
+    // If running in development, kill the server (keepalive if in production).
+    if (config.mode === `dev`) process.exit(0);
+});
+
 // Start and bind the webfront.
 require(`./src/server/webfront/index.js`);
