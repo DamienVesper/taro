f82c14251797a01af230086c3cdf0d431626d5b2
Damien Vesper <ldamienvesper@gmail.com>
2021-03-07 09:57:32 -0600

    Revert "add engine files"
    This reverts commit af4815c5a11019f82a375cb681e6a9a8a886b871.

--- engine/assets/IgeCuboidSmartTexture.js
+++
@@ -1,9 +0,0 @@
-let IgeCuboidSmartTexture = {
-    render: function (ctx, entity) {
-        let poly = entity.localIsoBoundsPoly();
-
-        ctx.strokeStyle = `#a200ff`;
-
-        poly.render(ctx);
-    }
-};
--- engine/components/physics/crash/CrashComponent.js
+++
@@ -1,9 +0,0 @@
-let CrashColliders = require(`crash-colliders`);
-
-let cc = new CrashColliders({});
-
-let crash = {};
-crash.b2Vec2 = cc.Vector;
-crash.Box = cc.Box;
-crash.createBody = cc.createBody;
-crash.onCollision = cc.onCollision;
