72146e35e1b72900f8674d7dad1f4191f61d64f0
Damien Vesper <ldamienvesper@gmail.com>
2021-03-07 10:16:13 -0600

    change renderer to a class

--- src/client/scripts/renderer.js
+++ src/client/scripts/renderer.js
@@ -1,12 +1,23 @@
-const renderer = new PIXI.Application({
-    width: 800,
-    height: 600,
-    antialias: true,
-    transparent: false,
-    autoResize: true,
-    forceCanvas: false
-});
+class Renderer {
+    constructor () {
+        this.renderer = new PIXI.Application({
+            width: 800,
+            height: 600,
+            antialias: true,
+            transparent: false,
+            autoResize: true,
+            forceCanvas: false
+        });
 
-renderer.scene = new PIXI.Container();
+        this.renderer.scene = new PIXI.Container();
+    }
 
-module.exports = renderer;
+    resize () {
+        const wW = window.innerWidth;
+        const wH = window.innerHeight;
+
+        this.renderer.resize(wW, wH);
+    }
+}
+
+module.exports = Renderer;
